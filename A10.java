1505a012748782b2f306cbd55016c46bf5dc8731
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-19 23:07:00 +0530

    Create A10.java

---
+++ A10.java
@@ -0,0 +1,16 @@
+public class A10 {
+
+		public static void main(String[] args){
+
+		char a = 'A';
+		// char b = 225;
+
+		a++;
+
+		System.out.println((int) a);
+		System.out.println(a);
+		
+		
+
+		}
+	}
