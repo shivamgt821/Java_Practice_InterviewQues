51b5f514294bec88d92556ac519d7378047232b4
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-20 00:41:14 +0530

    Create A49.java

---
+++ A49.java
@@ -0,0 +1,17 @@
+class Test56
+{
+	Test56(int x){
+		System.out.println(x);
+	}
+}
+public class A49 {
+	
+	Test56 t = new Test56(10);
+	A49(int i){
+		System.out.println(i);
+	}
+	public static void main(String[] args) {
+		A49 a = new A49(5);
+	}
+
+}
