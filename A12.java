2d06e0090d553b8a05f73d2f16add9ab252cbfea
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-19 23:08:26 +0530

    Create A12.java

---
+++ A12.java
@@ -0,0 +1,15 @@
+public class A12 {
+	
+
+        public static void main(String[] args){
+
+                int a = 9;
+
+               //float b = (float)a/2;       O/p-->4.5
+               float b = a/2;
+               System.out.println(b);
+
+         }
+}
+
+
