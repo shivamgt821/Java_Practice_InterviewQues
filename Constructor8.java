84392efdd40666577690fff99ce1cd38321a3bf4
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-22 23:16:42 +0530

    Create Constructor8.java

---
+++ Constructor8.java
@@ -0,0 +1,17 @@
+public class Constructor8 
+{
+	Constructor8(boolean a) 
+	{
+		System.out.println("MODEM="+ a );
+	}
+	Constructor8(float a) 
+	{
+		System.out.println("ROUTER="+ a );
+	}
+	
+	public static void main(String[] args) {
+		Constructor8 con1 = new Constructor8(50);
+		Constructor8 con2 = new Constructor8(false);
+	}
+	
+}
