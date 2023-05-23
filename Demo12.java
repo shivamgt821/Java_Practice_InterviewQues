b762cdc54e0f8634f62da7ec51af96810cd24062
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-23 22:26:44 +0530

    Create Demo12.java

---
+++ Demo12.java
@@ -0,0 +1,23 @@
+public class Demo12 
+{
+	int a = 20;
+	public static void test()
+	{
+		Demo12 d = new Demo12();
+		System.out.println(d.a);
+	}
+	static 
+	{
+		test();
+	}
+	{
+		
+	}
+	public static void main(String[] args) 
+	{
+
+	}
+	{
+		System.out.println("java");
+	}
+}
