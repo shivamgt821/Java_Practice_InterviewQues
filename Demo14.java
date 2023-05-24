06ceca0a576de29aed62ec48c97312fc807e1ef6
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-24 19:38:45 +0530

    Create Demo14.java

---
+++ Demo14.java
@@ -0,0 +1,15 @@
+class Test1 
+{
+	static void display() 
+	{
+		System.out.println("hey");
+	}
+}
+public class Demo14 {
+	public static void main(String[] args) {
+		Test1 t = null;
+		t.display();
+		// Test1.display();
+	}
+
+}
