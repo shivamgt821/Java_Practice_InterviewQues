3d4999fee40393e732342ed23f49e271cdf00962
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-24 19:38:07 +0530

    Create Demo13.java

---
+++ Demo13.java
@@ -0,0 +1,18 @@
+public class Demo13 
+{
+	int a = 20;
+	public void test()
+	{
+		System.out.println(a);
+	}
+	{	
+		a=10;
+		test();
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("java");
+	}
+}
+
