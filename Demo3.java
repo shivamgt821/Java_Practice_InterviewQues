08be4c3faf9cc6d805597cc2e010ac606188211c
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-25 22:50:53 +0530

    Create Demo3.java

---
+++ Demo3.java
@@ -0,0 +1,12 @@
+public class Demo3 {
+	public static void main(String[] args) {
+		int b = 10;
+		bike();					// a=20
+		System.out.println(b);	// b=10 a=20
+	}
+	public static void bike()
+	{
+		int a =20;
+		return;
+	}
+}
