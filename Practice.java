cb06b8bbf485addd68958b25a204e488a2fed1c1
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-03 22:51:10 +0530

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,10 @@
+public class Practice {
+
+	public static void main(String[] args) {
+		System.out.println(9+6-3*4/2%5);
+		System.out.println(-6+4*2);
+//		System.out.println((2+3)4);     // error due to not declaration
+		System.out.println(7+(3*5)*(4/2)+(5%3)-1);
+	}
+
+}
