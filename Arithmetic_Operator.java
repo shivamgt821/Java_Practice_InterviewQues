de819f83d8ae253451d13e788df70948a088312e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-04 19:14:04 +0530

    Create Arithmetic_Operator.java

---
+++ Arithmetic_Operator.java
@@ -0,0 +1,25 @@
+class Arithmetic_Operator
+{
+	public static void main(String[] args)
+	{
+		int a=123456;
+		int b=50;
+		int c=10;
+    	int d=a+b;
+		int e=a-b;
+		int f=a*c;
+		int g=a/c;
+		int h=a/b;
+		int i=a%c;
+		int j=a%b;
+
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(h);
+	System.out.println(i);
+	System.out.println(j);
+
+	}
+}
