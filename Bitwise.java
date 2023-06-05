582ce6514cc7e2eb6c34ee6896ea275eba353cc3
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-05 23:26:46 +0530

    Create Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,20 @@
+class Bitwise
+{
+
+		public static void main(String args []){
+				int a = 35;
+				int b = -35;
+				int c = a & b;
+				int d = a | b;
+				int e = 53;
+				System.out.println(c);
+				System.out.println(d);
+				System.out.println(~a);
+				System.out.println(~b);
+				System.out.println(a<<2);
+				System.out.println(a>>2);
+				System.out.println(a^b);
+				System.out.println(d>>2);
+				System.out.println(~e);
+			}
+}
