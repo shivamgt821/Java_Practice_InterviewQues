0c33890f78bc7964c1589307128f37e2c014de0e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-06 23:00:31 +0530

    Create Test0.java

---
+++ Test0.java
@@ -0,0 +1,18 @@
+class Book
+{
+	String title;
+	String author;
+    }
+class Test0
+{
+	public static void main(String[] args)
+	{
+		Book b1=new Book();
+		b1.title="c";
+		b1.author="denis";
+		System.out.println(b1.title);
+		System.out.println(b1.author);
+	}
+}
+		
+
