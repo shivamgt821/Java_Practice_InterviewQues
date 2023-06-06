d3856055cbf8599f98f1b7404141d6c618290215
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-06 22:59:59 +0530

    Create Bitwise2.java

---
+++ Bitwise2.java
@@ -0,0 +1,20 @@
+class Bitwise2
+{
+	public static void main(String[] args)
+	{
+	int a=52;
+	int b=-52;
+	System.out.println(a|b);
+	System.out.println(a&b);
+	System.out.println(a^b);
+	System.out.println(~a);
+	System.out.println(~b);
+	System.out.println(a<<2);
+	System.out.println(a>>2);
+	System.out.println(b<<2);
+	System.out.println(b>>2);
+	}
+}
+		
+
+
