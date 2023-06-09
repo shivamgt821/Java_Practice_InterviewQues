33063abb6654244a4332546e014a1bd54c2bf983
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-10 00:41:40 +0530

    Create For3.java

---
+++ For3.java
@@ -0,0 +1,10 @@
+class For3
+{
+	public static void main(String [] args){
+		int i=0;
+		for(i=1;i<=3;i++){
+		System.out.println("Hello : " +i); 
+	    }
+		System.out.println("Hello : " +i);
+	}
+}
