1c0019e0be15d5b75d712ef83a2781301d592ca8
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-10 00:42:20 +0530

    Create For5.java

---
+++ For5.java
@@ -0,0 +1,12 @@
+class For5
+{
+	public static void main(String [] args){
+		int i=1;
+		for(  ;  ;  ){
+		System.out.println("Hello : " +i); 
+	    }
+		System.out.println("Hello : " +i);
+	}
+}
+
+
