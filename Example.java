70908bcbed40cda3ceac728bcdb45ada228c6a73
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-12 21:39:29 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,20 @@
+class Example
+{
+ 	public static void m1()
+	{
+		int i=20;
+	System.out.println(i);
+	}
+
+	public static void main(String[] args)
+	{
+		int i=10;
+	
+	System.out.println(i); // same print st
+	m1();
+	System.out.println(i); // same print st dont confuse by method m1();
+
+	
+	}
+}
+
