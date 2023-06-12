69dd363539ee4fe2316f1a060eb442fe1b23c866
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-12 21:40:28 +0530

    Create OR.java

---
+++ OR.java
@@ -0,0 +1,12 @@
+class OR
+	{
+		public static void main(String[] args)
+		{
+			int a=14;
+			int b=15;
+			System.out.println(a++>14 | ++b>15);
+			System.out.println(a);
+			System.out.println(b);
+		}
+	}
+			
