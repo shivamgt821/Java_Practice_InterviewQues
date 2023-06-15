2c289cba319c8ccd168c19795dfabeed39a2aa21
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-16 00:31:20 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Test1
+{
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter the age of Shivam");
+		int a=scn.nextInt();
+	if(a>21){
+		System.out.println("Shivam is ready to get married");
+}
+		
+		System.out.println("Shivam is not ready for marriage");
+	}
+}
