bf41a82b77b5697c98dc47a8e40e8eb355f0e72b
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-17 22:00:05 +0530

    Create U.java

---
+++ U.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class U
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("enter a text");
+		Scanner scn=new Scanner(System.in);
+		String txt1=scn.next();
+		String txt2=scn.next();
+		String txt3=scn.next();
+
+		System.out.println(txt1);
+		System.out.println(txt2);
+		System.out.println(txt3);
+	}
+}
+	
