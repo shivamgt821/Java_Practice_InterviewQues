028872662561211b1e75640b7c0483de69367856
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-18 23:39:40 +0530

    Create RelationalOpr.java

---
+++ RelationalOpr.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class RelationalOpr
+{
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter two number");
+		int a=scn.nextInt();
+		int b=scn.nextInt();
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("a="+a+",b="+b);
+	}
+}
