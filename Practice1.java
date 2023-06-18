c4292e6af007b62de0565581641fbf278af3808e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-18 23:39:01 +0530

    Create Practice1.java

---
+++ Practice1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Practice1
+{
+	public static void main(String[] args)
+	{
+	  Scanner scn=new Scanner(System.in);	
+	  System.out.println("type 3 nums");
+	  int a=scn.nextInt();
+	  int b=scn.nextInt();
+	  int c=scn.nextInt();
+	  int d=(a+b+c);
+	  System.out.println(d);
+	}
+}
