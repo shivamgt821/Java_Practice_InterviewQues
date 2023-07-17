78ed80b9822859680221f0b257e9e2d336724de9
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-17 20:35:22 +0530

    Create LargestNumberOverGivenInteger.java

---
+++ LargestNumberOverGivenInteger.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LargestNumberOverGivenInteger {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter three integer : ");
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		int z = scn.nextInt();
+		
+		if(x > y && x > z) {
+			System.out.println("First number is greatest");
+		}
+		else if(y > x && y > z) {
+		System.out.println("Second number is greatest");
+		}
+		else if(z > x && z > y) {
+			System.out.println("Third number is greates");
+		}
+		else System.out.println("Entered Numbers are not distinct");
+	}
+
+}
