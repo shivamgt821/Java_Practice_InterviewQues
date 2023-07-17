77e50c32d1a2ccad85346fa7b5a4886a037580b2
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-17 20:36:34 +0530

    Update LargestNumberOverGivenInteger.java

--- LargestNumberOverGivenInteger.java
+++ LargestNumberOverGivenInteger.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class LargestNumberOverGivenInteger {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter three integer : ");
 		int x = scn.nextInt();
